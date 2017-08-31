5125c47361588449c0967987f1c499b179761976
Alkaloid <alkaloidoscope@gmail.com>
2017-08-31 15:02:52 +0200

    add mongolab

--- yelpcamp.js
+++ yelpcamp.js
@@ -17,8 +17,11 @@ var commentRoutes = require("./routes/comments"),
 campgroundRoutes  = require("./routes/campgrounds"),
 indexRoutes       = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useMongoClient: true });
+// local DB!
+// mongoose.connect("mongodb://localhost/yelp_camp", { useMongoClient: true });
 
+mongoose.connect("mongodb://Alkaloid:diolakla@ds111804.mlab.com:11804/ayelpcamp", { useMongoClient: true });
+// mongodb://Alkaloid:diolakla@ds111804.mlab.com:11804/ayelpcamp
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
