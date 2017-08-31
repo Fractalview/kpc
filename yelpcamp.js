811f593558eb8c37bed24e630a590f340e542bbb
Alkaloid <alkaloidoscope@gmail.com>
2017-08-31 14:30:34 +0200

    initial deploy commit

---
+++ yelpcamp.js
@@ -0,0 +1,58 @@
+
+var express   = require("express"),
+app           = express(),
+bodyParser    = require("body-parser"),
+mongoose      = require("mongoose"),
+Campground    = require("./models/campground"),
+Comment       = require("./models/comment"),
+seedDB        = require("./seeds.js"),
+passport      = require("passport"),
+LocalStrategy = require("passport-local"),
+User          = require("./models/user"),
+methodOverride = require("method-override"),
+flash          = require("connect-flash")
+
+// requiring routes
+var commentRoutes = require("./routes/comments"),
+campgroundRoutes  = require("./routes/campgrounds"),
+indexRoutes       = require("./routes/index")
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useMongoClient: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.set("view engine", "ejs");
+
+//seed the database
+// seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "terces",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(3000, function () {
+    console.log("YelpCamp server has started!");
+});
